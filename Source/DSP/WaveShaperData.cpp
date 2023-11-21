e8c879652a48e0ad533f8c6d080fe09728c7060c
Kyle Whaley <kitikdub@gmail.com>
2023-11-20 18:30:38 -0700

    Implemented first two wave shaper algos

--- Source/DSP/WaveShaperData.cpp
+++ Source/DSP/WaveShaperData.cpp
@@ -9,3 +9,97 @@
 */
 
 #include "WaveShaperData.h"
+
+void WaveShaper::prepareToPlay(juce::dsp::ProcessSpec& spec)
+{
+    inGain.reset();
+    inGain.prepare(spec);
+    inGain.setRampDurationSeconds(.05);
+
+    outGain.reset();
+    outGain.prepare(spec);
+    outGain.setRampDurationSeconds(.05);
+}
+
+void WaveShaper::process(juce::dsp::AudioBlock<float>& block, int channel)
+{
+    auto context = juce::dsp::ProcessContextReplacing<float>(block);
+
+    inGain.setGainDecibels(waveShaperInGainValue);
+    inGain.process(context);
+
+    for (int channel = 0; channel < block.getNumChannels(); ++channel)
+    {
+        switch (waveShaperTypeSelect)
+        {
+        case WaveShaper::sinusoidal:
+            processSinusoidal(channel, context);
+            break;
+
+        case WaveShaper::quadratic:
+            processQuadratic(channel, context);
+            break;
+
+        case WaveShaper::factor:
+            processFactor(channel, context);
+            break;
+
+        case WaveShaper::GloubiBoulga:
+            processGB(channel, context);
+            break;
+        }
+
+    }
+
+    outGain.setGainDecibels(waveShaperOutGainValue);
+    outGain.process(context);
+}
+
+void WaveShaper::updateParams(int typeSelect, std::vector<float>& factors, float inGain, float outGain, float mix)
+{
+    waveShaperTypeSelect = typeSelect;
+    waveShaperFactors = factors;
+    waveShaperInGainValue = inGain;
+    waveShaperOutGainValue = outGain;
+    waveshaperMix = mix;
+}
+
+void WaveShaper::processSinusoidal(int channel, juce::dsp::ProcessContextReplacing<float>& context)
+{
+    auto factor = waveShaperFactors[1];
+
+    auto z = juce::MathConstants<float>::pi * factor;
+    auto a = (1 / sin(z));
+    auto b = 1 / factor;
+
+    auto* channelInput = context.getInputBlock().getChannelPointer(channel);
+    auto* channelOutput = context.getOutputBlock().getChannelPointer(channel);
+
+    for (int s = 0; s < context.getInputBlock().getNumSamples(); ++s)
+    {
+        if (channelInput[s] > b)
+        {
+            channelOutput[s] = 1;
+        }
+        else
+        {
+            channelOutput[s] = sin(z * channelInput[s]) * a;
+        }
+
+    }
+}
+
+void WaveShaper::processQuadratic(int channel, juce::dsp::ProcessContextReplacing<float>& context)
+{
+
+    auto* channelInput = context.getInputBlock().getChannelPointer(channel);
+    auto* channelOutput = context.getOutputBlock().getChannelPointer(channel);
+
+    auto factor = waveShaperFactors[2];
+
+    for (int s = 0; s < context.getInputBlock().getNumSamples(); ++s)
+    {
+        channelOutput[s] = channelInput[s] * (abs(channelInput[s]) + factor) / (pow(channelInput[s], 2) + (factor - 1) * abs(channelInput[s]) + 1);
+
+    }
+}
--- Source/DSP/WaveShaperData.h
+++ Source/DSP/WaveShaperData.h
@@ -9,3 +9,34 @@
 */
 
 #pragma once
+#include <JuceHeader.h>
+struct WaveShaper 
+{
+    void prepareToPlay(juce::dsp::ProcessSpec& spec);
+    void process(juce::dsp::AudioBlock<float>& block, int channel);
+    void updateParams(int typeSelect, std::vector<float>& factors, float inGain, float outGain, float mix);
+
+private:
+
+    void processSinusoidal(int channel, juce::dsp::ProcessContextReplacing<float>& context);
+    void processQuadratic(int channel, juce::dsp::ProcessContextReplacing<float>& context);
+    void processFactor(int channel, juce::dsp::ProcessContextReplacing<float>& context);
+    void processGB(int channel, juce::dsp::ProcessContextReplacing<float>& context);
+
+    enum WaveShaperSelect {
+        none,
+        sinusoidal,
+        quadratic,
+        factor,
+        GloubiBoulga
+    };
+
+    juce::dsp::Gain<float> inGain;
+    juce::dsp::Gain<float> outGain;
+
+    int waveShaperTypeSelect;
+    std::vector<float> waveShaperFactors;
+    float waveShaperInGainValue;
+    float waveShaperOutGainValue;
+    float waveshaperMix;
+};
